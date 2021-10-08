27da0717a64deb1782a22b6074bdbb937001907f
Raghav <bathla.raghav5@gmail.com>
2021-10-09 00:54:40 +0530

    [Raghav Make Move of user added]

--- src/ticTacToe/TicTacToeClass.java
+++ src/ticTacToe/TicTacToeClass.java
@@ -27,9 +27,23 @@ public class TicTacToeClass {
             computer = 'X';
         }
     }
-    static private void showBoard(char[] board) {
+    private void makeMove(char[] board, char user) {
+        System.out.println("Enter the position from 1 to 9");
+        Scanner scanner = new Scanner(System.in);
+        int move = scanner.nextInt();
+        if (board[move] == ' ') {
+            board[move] = user;
+
+        } else {
+            System.out.println("Not empty");
+
+        }
+
+    }
+     private void showBoard(char[] board) {
         System.out.println(board[1] + "|" + board[2] + "|" + board[3]);
         System.out.println(board[4] + "|" + board[5] + "|" + board[6]);
         System.out.println(board[7] + "|" + board[8] + "|" + board[9]);
     }
+
 }
