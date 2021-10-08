a10da7265e7bdda0dc6cb9885acc2d48f85d38c4
Raghav <bathla.raghav5@gmail.com>
2021-10-08 17:05:20 +0530

    [Raghav] Added Ask User to choose X or O

--- src/ticTacToe/TicTacToeClass.java
+++ src/ticTacToe/TicTacToeClass.java
@@ -1,8 +1,11 @@
 package ticTacToe;
 
+import java.util.Scanner;
+
 public class TicTacToeClass {
     private char board[] = new char[10];
-
+    private char user;
+     private char computer;
      private char[] assign() {
 
         for (int i = 1; i <= board.length - 1; i++) {
@@ -10,4 +13,19 @@ public class TicTacToeClass {
         }
         return board;
     }
+
+     private void askUser() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Choose between X or O");
+
+        System.out.println("Enter X for X or O for O");
+        user = scanner.next().charAt(0);
+        if (user == 'X')
+            computer = 'O';
+        else {
+            user = 'O';
+            computer = 'X';
+        }
+    }
+
 }
