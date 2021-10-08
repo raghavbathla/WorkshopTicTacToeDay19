5ac358d788b7e8f8b69d9dd78d62a392b435caa4
Raghav <bathla.raghav5@gmail.com>
2021-10-08 17:19:12 +0530

    [Raghav] Added showBoard

--- src/ticTacToe/TicTacToeClass.java
+++ src/ticTacToe/TicTacToeClass.java
@@ -13,7 +13,11 @@ public class TicTacToeClass {
         }
         return board;
     }
-
+     private void showBoard(char[] board) {
+        System.out.println(board[1] + "|" + board[2] + "|" + board[3]);
+        System.out.println(board[4] + "|" + board[5] + "|" + board[6]);
+        System.out.println(board[7] + "|" + board[8] + "|" + board[9]);
+    }
      private void askUser() {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Choose between X or O");
