e2cf0b0cff14fb375cecfea66327e7676513f1d2
Raghav <bathla.raghav5@gmail.com>
2021-10-08 17:29:46 +0530

    [Raghav] Show Board added

--- src/ticTacToe/TicTacToeClass.java
+++ src/ticTacToe/TicTacToeClass.java
@@ -27,5 +27,9 @@ public class TicTacToeClass {
             computer = 'X';
         }
     }
-
+    static private void showBoard(char[] board) {
+        System.out.println(board[1] + "|" + board[2] + "|" + board[3]);
+        System.out.println(board[4] + "|" + board[5] + "|" + board[6]);
+        System.out.println(board[7] + "|" + board[8] + "|" + board[9]);
+    }
 }
