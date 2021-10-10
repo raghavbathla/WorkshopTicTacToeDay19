2720a440c40e6ea6ee932aa8bf56dadb0386223a
Raghav <bathla.raghav5@gmail.com>
2021-10-10 15:35:54 +0530

    [Raghav] Tic tac toe completed

--- src/ticTacToe/TicTacMain.java
+++ src/ticTacToe/TicTacMain.java
@@ -2,6 +2,16 @@ package ticTacToe;
 
 public class TicTacMain {
     public static void main(String[] args) {
+TicTacToeClass ticTacToeClass = new TicTacToeClass();
+ticTacToeClass.askUser();
+char [] board = ticTacToeClass.assign();
+char user = ticTacToeClass.user;
+char computer = ticTacToeClass.computer;
+ticTacToeClass.showBoard(board);
+ticTacToeClass.firstMove(board,user,computer);
+ticTacToeClass.showBoard(board);
+ticTacToeClass.startGame(board,user,computer);
+
 
     }
 }
